b7c8e548e117e429bfa2478e2338aa835ff69286
lantigua <lantigua2q@gmail.com>
2025-05-29 07:49:57 +0100

    cambio css de table

---
+++ index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html>
+<head>
+<!--Añaddido para la carga de DataTables CSS -->
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.7/css/jquery.dataTables.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.5.0/css/responsive.dataTables.min.css">
+    
+    <meta charset="utf-8">
+    <title>CRUD XML</title>
+    <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
+</head>
+<body>
+<div class="container">
+    <h1 class="page-header text-center">CRUD XML con PHP, Bootstrap,JQuery y DataTables.</h1>
+    <div class="row">
+        <div class="col-sm-8 col-sm-offset-2">
+            <a href="#addnew" class="btn btn-primary" data-toggle="modal"><span class="glyphicon glyphicon-plus"></span> New</a>
+            <?php
+                session_start();
+                if(isset($_SESSION['message'])){
+                    ?>
+                    <div class="alert alert-info text-center" style="margin-top:20px;">
+                        <?php echo $_SESSION['message']; ?>
+                    </div>
+                    <?php
+
+                    unset($_SESSION['message']);
+                }
+            ?>
+            <table id="tabla" class="table table-success  table-striped" style="margin-top:20px;">
+                <thead>
+                    <th>UserID</th>
+                    <th>Firstname</th>
+                    <th>Lastname</th>
+                    <th>Address</th>
+                    <th>Action</th>
+                </thead>
+                <tbody>
+                    <?php
+                    //load xml file
+                    $file = simplexml_load_file('files/members.xml');
+
+                    foreach($file->user as $row){
+                        ?>
+                        <tr>
+                            <td><?php echo $row->id; ?></td>
+                            <td><?php echo $row->firstname; ?></td>
+                            <td><?php echo $row->lastname; ?></td>
+                            <td><?php echo $row->address; ?></td>
+                            <td>
+                                <a href="#edit_<?php echo $row->id; ?>" data-toggle="modal" class="btn btn-success btn-sm"><span class="glyphicon glyphicon-edit"></span> Edit</a>
+                                <a href="#delete_<?php echo $row->id; ?>" data-toggle="modal" class="btn btn-danger btn-sm"><span class="glyphicon glyphicon-trash"></span> Delete</a>
+                            </td>
+                            <?php include('edit_delete_modal.php'); ?>
+                        </tr>
+                        <?php
+                    }
+
+                    ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+<?php include('add_modal.php'); ?>
+
+ <!-- jQuery (requerido por DataTables y Bootstrap) -->
+ <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+
+ <!-- DataTables JS -->
+ <script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>
+ <script src="https://cdn.datatables.net/responsive/2.5.0/js/dataTables.responsive.min.js"></script>
+
+ <!-- Bootstrap JS -->
+ <script src="bootstrap/js/bootstrap.min.js"></script>
+ 
+<script>
+$(document).ready(function() {
+  $('#tabla').DataTable();
+});
+</script>
+
+
+</body>
+</html>
